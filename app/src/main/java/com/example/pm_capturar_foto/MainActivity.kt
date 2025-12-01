b244069259f512c586108bcf26e834ffa06b197a
Ruben Leis <rubenleisfp@gmail.com>
2025-12-01 13:33:19 +0100

    Maqueta vacia. Tiene ya incoporada los permisos y librerias necesarias

--- app/src/main/java/com/example/pm_capturar_foto/MainActivity.kt
+++ app/src/main/java/com/example/pm_capturar_foto/MainActivity.kt
@@ -45,78 +45,14 @@ class MainActivity : ComponentActivity() {
     // Función onCreate: se llama cuando se crea la actividad
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Habilita la barra de aplicaciones para que se extienda hasta los bordes de la pantalla
-        enableEdgeToEdge()
-        // Configura el contenido de la actividad
-        setContent {
-            // Crea un tema de la aplicación y lo aplica
-            Pm_capturar_fotoTheme {
-                // Crea una superficie que rellena todo el espacio disponible y utiliza el color de fondo del tema
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    // Muestra la vista de la cámara
-                    CamaraView()
-                }
-            }
-        }
+        throw UnsupportedOperationException("A implementar por el estudiante")
     }
 }
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun CamaraView() {
-    // Solicita los permisos de la cámara
-    val permissions = rememberMultiplePermissionsState(
-        permissions = listOf(Manifest.permission.CAMERA)
-    )
-
-    // Obtiene el contexto de la actividad
-    val context = LocalContext.current
-    // Crea un controlador de cámara que se vincula a la actividad
-    val camaraController = remember { LifecycleCameraController(context) }
-    // Obtiene el propietario de la vida de la actividad
-    val lifecycle = LocalLifecycleOwner.current
-
-    // Obtiene el directorio de imágenes públicas
-    val directorio = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).absoluteFile
-
-    // Verifica los permisos cuando se crea el Composable
-    LaunchedEffect(key1 = Unit) {
-        permissions.launchMultiplePermissionRequest()
-    }
-
-    // Muestra un botón flotante para tomar una foto
-    Scaffold(modifier = Modifier.fillMaxSize(),
-        floatingActionButton = {
-            FloatingActionButton(
-                onClick = {
-                    // Crea un ejecutor para manejar la toma de la foto
-                    val executor = ContextCompat.getMainExecutor(context)
-                    // Toma una foto y la guarda en el directorio de imágenes públicas
-                    tomarFoto(camaraController, executor, directorio)
-                }
-            ) {
-                // Muestra un icono de cámara
-                Icon(
-                    painterResource(id = R.drawable.icon_camera),
-                    tint = Color.White,
-                    contentDescription = null
-                )
-            }
-        },
-        floatingActionButtonPosition = FabPosition.Center
-    ) {
-        // Muestra la vista de la cámara si se han concedido los permisos
-        if (permissions.allPermissionsGranted) {
-            CamaraComposable(
-                camaraController = camaraController,
-                lifecycleOwner = lifecycle,
-                modifier = Modifier.padding(it)
-            )
-        } else {
-            // Muestra un mensaje si no se han concedido los permisos
-            Text(text = "Permisos no concedidos", modifier = Modifier.padding(it))
-        }
-    }
+    throw UnsupportedOperationException("A implementar por el estudiante")
 
 }
 
@@ -126,22 +62,7 @@ fun CamaraComposable(
     lifecycleOwner: LifecycleOwner,
     modifier: Modifier
 ) {
-    // Vincula el controlador de cámara a la vida de la actividad
-    camaraController.bindToLifecycle(lifecycleOwner)
-    // Muestra la vista de previsualización de la cámara
-    AndroidView(
-        modifier = modifier,
-        factory = {
-            val previaView = PreviewView(it).apply {
-                layoutParams = ViewGroup.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT
-                )
-            }
-            previaView.controller = camaraController
-            previaView
-        }
-    )
+    throw UnsupportedOperationException("A implementar por el estudiante")
 }
 
 // Función para tomar una foto y guardarla en el directorio de imágenes públicas
@@ -150,31 +71,7 @@ private fun tomarFoto(
     executor: Executor,
     directorio: File
 ) {
-    // Crea un archivo temporal para la foto
-    val image = File.createTempFile("img_", ".png", directorio)
-    // Crea opciones para guardar la foto en el archivo temporal
-    val outputDirectory = ImageCapture.OutputFileOptions.Builder(image).build()
-    // Toma la foto y guarda el resultado en el archivo temporal
-    camaraController.takePicture(
-        outputDirectory,
-        executor,
-        object : ImageCapture.OnImageSavedCallback {
-            // Función que se llama cuando se guarda la foto
-            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                // Muestra un mensaje en la consola para indicar que se ha guardado la foto
-                println("foto_tomada")
-                // Muestra la URI de la foto guardada en la consola
-                println(outputFileResults.savedUri)
-            }
-
-            override fun onError(exception: ImageCaptureException) {
-                println(exception.toString())
-            }
-
-
-
-        }
-    )
+   throw UnsupportedOperationException("A implementar por el estudiante")
 }
 
 // Función de previsualización para mostrar la vista de la cámara en el diseñador
